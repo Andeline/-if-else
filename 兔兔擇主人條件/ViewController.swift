7d76e8d1e98a29eb4c4c6d54d47977e1b7f24d5e
Andeline <andelineya@gmail.com>
2021-05-08 00:04:13 +0800

    兔兔條件

--- 兔兔擇主人條件/ViewController.swift
+++ 兔兔擇主人條件/ViewController.swift
@@ -9,11 +9,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var whoTextField: UITextField!
+    @IBOutlet weak var loveSwitch: UISwitch!
+    @IBOutlet weak var strengthSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var frequencySlider: UISlider!
+    @IBOutlet weak var touchNumberLabel: UILabel!
+    @IBOutlet weak var forgotBtn: UIButton!
+    @IBOutlet weak var onceBtn: UIButton!
+    @IBOutlet weak var manyBtn: UIButton!
+    
+    @IBOutlet weak var watchImageView: UIImageView!
+    @IBOutlet weak var loveView: UIView!
+    @IBOutlet weak var hateView: UIView!
+    
+    var snakeFrequency = ""
+    var whoText = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        hateView.frame = CGRect(x: 37, y: 485, width: 341, height: 400)
+        loveView.isHidden = true
+        hateView.isHidden = true
+        watchImageView.isHidden = false
     }
 
-
+    @IBAction func closeKeyboard(_ sender: UITextField) {
+        whoText = whoTextField.text!
+    }
+    
+    @IBAction func getTouchNumber(_ sender: UISlider) {
+        touchNumberLabel.text = "\(Int(sender.value))"
+        print("\(Int(sender.value))")
+    }
+    
+    @IBAction func getSnakeFrequencyValue(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            snakeFrequency = "unacceptable"
+        case 1:
+            snakeFrequency = "ok"
+        case 2:
+            snakeFrequency = "ok"
+        default:
+            break
+        }
+        print("\(snakeFrequency)")
+    }
+    
+    @IBAction func test(_ sender: UIButton) {
+        
+        let whoText = whoTextField.text!
+ 
+        if whoText.contains("媽") || whoText.contains("佳") || whoText.contains("mom"), loveSwitch.isOn, strengthSegmentedControl.selectedSegmentIndex == 0, frequencySlider.value >= 2, snakeFrequency == "ok" {
+            loveView.isHidden = false
+            hateView.isHidden = true
+            watchImageView.isHidden = true
+        } else {
+            hateView.isHidden = false
+            loveView.isHidden = true
+            watchImageView.isHidden = true
+        }
+    }
+    @IBAction func clear(_ sender: UIButton) {
+        
+        whoTextField.text = ""
+        loveSwitch.isOn = false
+        strengthSegmentedControl.selectedSegmentIndex = 0
+        frequencySlider.value = 0
+        touchNumberLabel.text = "0"
+        loveView.isHidden = true
+        hateView.isHidden = true
+        watchImageView.isHidden = false
+    }
 }
 
